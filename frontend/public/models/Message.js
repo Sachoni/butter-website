9657e14c87561912758c774c041590f01354a703
Sachoni <wamukotajohnmark@gmail.com>
2025-09-16 02:09:04 +0300

    Vite React + Vercel API + MongoDB setup

--- frontend/public/models/Message.js
+++
@@ -1,6 +0,0 @@
-// api/models/Message.js
-import mongoose from "mongoose";
-
-const MessageSchema = new mongoose.Schema({ text: String }, { timestamps: true });
-
-export default mongoose.models.Message || mongoose.model("Message", MessageSchema);
